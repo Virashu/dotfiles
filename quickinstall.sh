13ae1672aaab28ca5e088e7ac501595089a50369
Virashu <svladichek@gmail.com>
2022-11-07 23:02:58 +0300

    fix

--- quickinstall.sh
+++ quickinstall.sh
@@ -12,7 +12,7 @@ git init --bare $HOME/.config
 alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 
-if [[ "$SHELL" == "/usr/bin/bash" ]]; then
+if [[ "$SHELL" == "/bin/bash" ]]; then
 	echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'" >> .bashrc
 elif [[ "$SHELL" == "/usr/bin/zsh" ]]; then
 	echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'" >> .zshrc
@@ -22,3 +22,10 @@ else
 fi
 
 echo "Initialized successfully!"
+
+read -p "Wish to move dotfiles? [Y/n]" ans
+if [[ $ans == 'n' ]] || [[ $ans == 'N' ]]; then
+	echo "Quitting..."
+	exit
+if [[ $ans == 'y' ]] || [[ $ans == 'Y' ]] || [[ $ans == '' ]]; then
+	cp ./!(quickinstall.sh) ~/
