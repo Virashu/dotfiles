e5cd3c7b7f18c5ba3c3cfc0517c37da4f32d3883
Virashu <svladichek@gmail.com>
2022-11-07 21:38:59 +0300

    qi

---
+++ quickinstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Starting initialization"
+
+if ! command -v git &> /dev/null
+then
+	echo "git is not installed"
+	exit
+fi
+
+git init --bare $HOME/.config
+alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+
+if [[ "$SHELL" == "/usr/bin/bash" ]]; then
+	echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'" >> .bashrc
+elif [[ "$SHELL" == "/usr/bin/zsh" ]]; then
+	echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'" >> .zshrc
+else
+	echo "ERROR"
+	exit
+fi
+
+echo "Initialized successfully!"
